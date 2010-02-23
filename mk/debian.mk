213c8b15127e4531db4297cd4d4b741efc5e34f0
Vegard Bønes <vegard.bones@met.no>
2010-02-23 11:56:02 +0000

    removed -nc from debian build

--- mk/debian.mk
+++ mk/debian.mk
@@ -7,21 +7,30 @@ PKG_DIR = $(PACKAGE)-$(VERSION)
 DEBIAN_DIR = $(PKG_DIR)/debian
 DEBIAN_PACKAGE = $(PACKAGE)_$(VERSION)
 ARCH = `dpkg-architecture -qDEB_HOST_ARCH_CPU`
-DEBIAN_PACKAGE_NAME = `head -n1 $(top_srcdir)/debian_files/changelog | sed "s/ (/_/" | sed "s/).*//"`_$(ARCH).deb
+DEBIAN_PACKAGE_NAME_BASE = `head -n1 $(top_srcdir)/debian_files/changelog | sed "s/ (/_/" | sed "s/).*//"`
+DEBIAN_PACKAGE_NAME = $(DEBIAN_PACKAGE_NAME_BASE)_$(ARCH).deb
+DEBIAN_SOURCE_PACKAGE_NAME = $(DEBIAN_PACKAGE_NAME_BASE).dsc
 
-prepare-debian: dist clean-debian
+dist-debian: dist clean-debian
 	tar xvzf $(PKG_DIR).tar.gz
-	mkdir -p $(DEBIAN_DIR)
 	cp $(PKG_DIR).tar.gz $(DEBIAN_PACKAGE).orig.tar.gz
 
-update-debian:
-	rm -rf $(DEBIAN_DIR)/*
+prepare-debian:
+	rm -rf $(DEBIAN_DIR)
+	mkdir -p $(DEBIAN_DIR)
 	cp -r $(top_srcdir)/debian_files/* $(DEBIAN_DIR)
 	chmod 774 $(DEBIAN_DIR)/rules
-	cd $(PKG_DIR) && dpkg-buildpackage -rfakeroot -us -uc -nc
-	lintian $(DEBIAN_PACKAGE_NAME)
+	debconf-updatepo --podir=$(DEBIAN_DIR)/po
 
-debian: prepare-debian update-debian
+update-debian: prepare-debian
+	cd $(PKG_DIR) && dpkg-buildpackage -rfakeroot -us -uc -nc
+	lintian $(DEBIAN_PACKAGE_NAME) $(DEBIAN_SOURCE_PACKAGE_NAME)
+
+build-debian:
+	cd $(PKG_DIR) && dpkg-buildpackage -rfakeroot -us -uc
+	lintian $(DEBIAN_PACKAGE_NAME) $(DEBIAN_SOURCE_PACKAGE_NAME)
+
+debian: dist-debian prepare-debian build-debian
 
 clean-debian:
 	debclean
